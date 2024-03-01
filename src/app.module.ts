860c25905566c88d8e1fb3f551a7a6c47d98983b
Mark Bennett <mark@vira.health>
2024-03-01 15:20:35 +0000

    Convert to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [ItemsController],
-  providers: [ItemsService],
+  imports: [ItemsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
